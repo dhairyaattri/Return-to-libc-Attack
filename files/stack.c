d0284cf0dabf53f227c60bed1f3f1e35775d20ea
Dhairya <dhairya@Dhairyas-MBP.home>
2022-10-08 14:13:58 +0530

    first commit

---
+++ files/stack.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int vul_func(char *str)
+{
+	char buffer[50];
+	strcpy (buffer, str);
+	return 1;
+}
+
+int main (int argc, char **argv)
+{
+	char str[240];
+	FILE *badfile;
+
+	badfile = fopen ("badfile", "r");
+	fread(str, sizeof(char), 200, badfile);
+	vul_func(str);
+
+	printf("Returned Properly\n");
+	return 1;
+}
